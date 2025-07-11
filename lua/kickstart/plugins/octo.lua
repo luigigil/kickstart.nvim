7cfd8b55137d52e2e4346adfdde85629576f32fb
Luigi Nunes Gil <luiginunes.gil@coinbase.com>
2025-07-11 18:04:31 -0300

    feat: add support for custom octo on work pc

--- lua/kickstart/plugins/octo.lua
+++ lua/kickstart/plugins/octo.lua
@@ -1,13 +1,31 @@
 return {
-	"pwntester/octo.nvim",
-	requires = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("octo").setup({
-			use_local_fs = true,
-		})
-	end,
+  {
+    'pwntester/octo.nvim',
+    cond = not vim.env.WORK,
+    requires = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('octo').setup {
+        use_local_fs = true,
+      }
+    end,
+  },
+  {
+    'octo.custom.nvim',
+    dir = vim.fn.stdpath 'config' .. '/lua/custom/plugins/octo.custom.nvim',
+    cond = vim.env.WORK,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('octo').setup {
+        use_local_fs = true,
+      }
+    end,
+  },
 }
