bed32bf67628edc4f19f0283aa63fe361c9ac1a8
luigi <luigi.gil@gmail.com>
2025-09-02 18:43:47 -0300

    add markview checkbox

--- lua/kickstart/plugins/markview.lua
+++ lua/kickstart/plugins/markview.lua
@@ -6,16 +6,53 @@ return {
   -- For `nvim-treesitter` users.
   priority = 49,
 
+  config = function()
+    require('markview.extras.checkboxes').setup {
+      --- Default checkbox state(used when adding checkboxes).
+      ---@type string
+      default = 'X',
+
+      --- Changes how checkboxes are removed.
+      ---@type
+      ---| "disable" Disables the checkbox.
+      ---| "checkbox" Removes the checkbox.
+      ---| "list_item" Removes the list item markers too.
+      remove_style = 'disable',
+
+      --- Various checkbox states.
+      ---
+      --- States are in sets to quickly change between them
+      --- when there are a lot of states.
+      ---@type string[][]
+      states = {
+        { ' ', '/', 'X' },
+        { '<', '>' },
+        { '?', '!', '*' },
+        { '"' },
+        { 'l', 'b', 'i' },
+        { 'S', 'I' },
+        { 'p', 'c' },
+        { 'f', 'k', 'w' },
+        { 'u', 'd' },
+      },
+    }
+  end,
+
   dependencies = {
     'nvim-tree/nvim-web-devicons',
     'saghen/blink.cmp',
   },
-  opts = {
-    preview = {
-      filetypes = { 'markdown', 'norg', 'rmd', 'org', 'vimwiki', 'Avante' },
-    },
-    max_length = 99999,
-  },
+  opts = function()
+    return {
+      preview = {
+        filetypes = { 'markdown', 'norg', 'rmd', 'org', 'vimwiki', 'Avante' },
+      },
+      max_length = 99999,
+      markdown = {
+        headings = require('markview.presets').headings.glow,
+      },
+    }
+  end,
   keys = {
     {
       '<leader>mt',
@@ -32,6 +69,18 @@ return {
       end,
       mode = 'n',
       desc = '[M]arkview [s]plit toggle',
+    },
+    {
+      '<leader>mct',
+      '<cmd>Checkbox toggle<CR>',
+      mode = 'n',
+      desc = '[M]arkdown [c]heckbox [t]oggle',
+    },
+    {
+      '<leader>mci',
+      '<cmd>Checkbox interactive<CR>',
+      mode = 'n',
+      desc = '[M]arkdown [c]heckbox [i]nteractive',
     },
   },
 }
